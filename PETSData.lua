924b9d4fb0e3dc108e2d010221bdf3ed497f150a
mammothkung <tongmo.jingithub@gmail.com>
2025-03-31 05:33:01 +0700

    Create PETSData.lua

---
+++ PETSData.lua
@@ -0,0 +1,34 @@
+return {
+  Image = {
+        ["Soondoo"] = {
+            Image = "83952165808326",
+            BackgroundTransparency = 0,
+            BackgroundColor3 = Color3.fromRGB(45,45,45)
+        },
+        ["Gonshee"] = {
+            Image = "82914824031307",
+            BackgroundTransparency = 0,
+            BackgroundColor3 = Color3.fromRGB(45,45,45)
+        },
+        ["Daek"] = {
+            Image = "112717350204803",
+            BackgroundTransparency = 0,
+            BackgroundColor3 = Color3.fromRGB(45,45,45)
+        },
+        ["LongIn"] = {
+            Image = "107186645910420",
+            BackgroundTransparency = 0,
+            BackgroundColor3 = Color3.fromRGB(45,45,45)
+        },
+        ["Anders"] = {
+            Image = "110844883730834",
+            BackgroundTransparency = 0,
+            BackgroundColor3 = Color3.fromRGB(45,45,45)
+        },
+        ["Largalgan"] = {
+            Image = "85957675205976",
+            BackgroundTransparency = 0,
+            BackgroundColor3 = Color3.fromRGB(45,45,45)
+        }
+    }
+}
