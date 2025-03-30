3e6e6a317a3187f0016596b3dbf99323964d3664
mammothkung <tongmo.jingithub@gmail.com>
2025-03-31 06:30:16 +0700

    Update PETSData.lua

--- PETSData.lua
+++ PETSData.lua
@@ -1,140 +1,140 @@
 return {
-  Pets = {
-        "Soondoo",
-        "Gonshee",
-        "Daek",
-        "LongIn",
-        "Anders",
-        "Largalgan",
-        "Vermillion",
-        "Monarch",
-        "Ant Soldier",
-        "Ant Queen",
-        "Ziru",
-        "Snake Man",
-        "Blossom",
-        "Black Crow",
-        "Dor"
-  },
-  Image = {
-        ["Soondoo"] = {
-            Image = "83952165808326",
-            BackgroundTransparency = 0,
-            BackgroundColor3 = Color3.fromRGB(45,45,45)
-        },
-        ["Gonshee"] = {
-            Image = "82914824031307",
-            BackgroundTransparency = 0,
-            BackgroundColor3 = Color3.fromRGB(45,45,45)
-        },
-        ["Daek"] = {
-            Image = "112717350204803",
-            BackgroundTransparency = 0,
-            BackgroundColor3 = Color3.fromRGB(45,45,45)
-        },
-        ["LongIn"] = {
-            Image = "107186645910420",
-            BackgroundTransparency = 0,
-            BackgroundColor3 = Color3.fromRGB(45,45,45)
-        },
-        ["Anders"] = {
-            Image = "110844883730834",
-            BackgroundTransparency = 0,
-            BackgroundColor3 = Color3.fromRGB(45,45,45)
-        },
-        ["Largalgan"] = {
-            Image = "85957675205976",
-            BackgroundTransparency = 0,
-            BackgroundColor3 = Color3.fromRGB(45,45,45)
-        },
-        ["Vermillion"] = {
-            Image = "120675730326685",
-            BackgroundTransparency = 0,
-            BackgroundColor3 = Color3.fromRGB(45,45,45)
-        },
-        ["Monarch"] = {
-            Image = "77293116688131",
-            BackgroundTransparency = 0,
-            BackgroundColor3 = Color3.fromRGB(45,45,45)
-        },
-        ["Ant Soldier"] = {
-            Image = "103369685912011",
-            BackgroundTransparency = 0,
-            BackgroundColor3 = Color3.fromRGB(45,45,45)
-        },
-        ["Ant Queen"] = {
-            Image = "73358558923030",
-            BackgroundTransparency = 0,
-            BackgroundColor3 = Color3.fromRGB(45,45,45)
-        },
-        ["Ziru"] = {
-            Image = "118102358638644",
-            BackgroundTransparency = 0,
-            BackgroundColor3 = Color3.fromRGB(45,45,45)
-        },
-        ["Snake Man"] = {
-            Image = "118834985644786",
-            BackgroundTransparency = 0,
-            BackgroundColor3 = Color3.fromRGB(45,45,45)
-        },
-        ["Blossom"] = {
-            Image = 85463295750056",
-            BackgroundTransparency = 0,
-            BackgroundColor3 = Color3.fromRGB(45,45,45)
-        },
-        ["Black Crow"] = {
-            Image = "129187553444710",
-            BackgroundTransparency = 0,
-            BackgroundColor3 = Color3.fromRGB(45,45,45)
-        },
-        ["Dor"] = {
-            Image = "88039038571553",
-            BackgroundTransparency = 0,
-            BackgroundColor3 = Color3.fromRGB(45,45,45)
-        }
-    },
-  HP = {
-        ["Soondoo"] = "HP: 50",
-        ["Gonshee"] = "HP: 500",
-        ["Daek"] = "HP: 5K",
-        ["LongIn"] = "HP: 10K",
-        ["Anders"] = "HP: 15K",
-        ["Largalgan"] = "HP: 30K",
-        ["Snake Man"] = "HP: 99K",
-        ["Blossom"] = "HP: 840K"
-  },
-  Island = {
-        ["Soondoo"] = "Leveling City",
-        ["Gonshee"] = "Leveling City",
-        ["Daek"] = "Leveling City",
-        ["LongIn"] = "Leveling City",
-        ["Anders"] = "Leveling City",
-        ["Largalgan"] = "Leveling City",
-        ["Vermillion"] = "Leveling City",
-        ["Monarch"] = "Leveling City",
-        ["Ant Soldier"] = "Dedu Island",
-        ["Ant Queen"] = "Dedu Island",
-        ["Ziru"] = "Dedu Island",
-        ["Snake Man"] = "Grass Village",
-        ["Blossom"] = "Grass Village",
-        ["Black Crow"] = "Grass Village",
-        ["Dor"] = "Grass Village"
-  },
-  Rarity = {
-        ["Soondoo"] = "Rarity: Common",
-        ["Gonshee"] = "Rarity: Common",
-        ["Daek"] = "Rarity: Common",
-        ["LongIn"] = "Rarity: Common",
-        ["Anders"] = "Rarity: Common",
-        ["Largalgan"] = "Rarity: Common",
-        ["Vermillion"] = "Rarity: Epic",
-        ["Monarch"] = "Rarity: Legendary",
-        ["Ant Soldier"] = "Rarity: Common",
-        ["Ant Queen"] = "Rarity: Epic",
-        ["Ziru"] = "Rarity: Legendary",
-        ["Snake Man"] = "Rarity: Common",
-        ["Blossom"] = "Rarity: Common",
-        ["Black Crow"] = "Rarity: Common",
-        ["Dor"] = "Rarity: Epic"
+	Pets = {
+		  "Soondoo",
+		  "Gonshee",
+		  "Daek",
+		  "LongIn",
+		  "Anders",
+		  "Largalgan",
+		  "Vermillion",
+		  "Monarch",
+		  "Ant Soldier",
+		  "Ant Queen",
+		  "Ziru",
+		  "Snake Man",
+		  "Blossom",
+		  "Black Crow",
+		  "Dor"
+	},
+	Image = {
+		  ["Soondoo"] = {
+			  Image = "83952165808326",
+			  BackgroundTransparency = 0,
+			  BackgroundColor3 = Color3.fromRGB(45,45,45)
+		  },
+		  ["Gonshee"] = {
+			  Image = "82914824031307",
+			  BackgroundTransparency = 0,
+			  BackgroundColor3 = Color3.fromRGB(45,45,45)
+		  },
+		  ["Daek"] = {
+			  Image = "112717350204803",
+			  BackgroundTransparency = 0,
+			  BackgroundColor3 = Color3.fromRGB(45,45,45)
+		  },
+		  ["LongIn"] = {
+			  Image = "107186645910420",
+			  BackgroundTransparency = 0,
+			  BackgroundColor3 = Color3.fromRGB(45,45,45)
+		  },
+		  ["Anders"] = {
+			  Image = "110844883730834",
+			  BackgroundTransparency = 0,
+			  BackgroundColor3 = Color3.fromRGB(45,45,45)
+		  },
+		  ["Largalgan"] = {
+			  Image = "85957675205976",
+			  BackgroundTransparency = 0,
+			  BackgroundColor3 = Color3.fromRGB(45,45,45)
+		  },
+		  ["Vermillion"] = {
+			  Image = "120675730326685",
+			  BackgroundTransparency = 0,
+			  BackgroundColor3 = Color3.fromRGB(45,45,45)
+		  },
+		  ["Monarch"] = {
+			  Image = "77293116688131",
+			  BackgroundTransparency = 0,
+			  BackgroundColor3 = Color3.fromRGB(45,45,45)
+		  },
+		  ["Ant Soldier"] = {
+			  Image = "103369685912011",
+			  BackgroundTransparency = 0,
+			  BackgroundColor3 = Color3.fromRGB(45,45,45)
+		  },
+		  ["Ant Queen"] = {
+			  Image = "73358558923030",
+			  BackgroundTransparency = 0,
+			  BackgroundColor3 = Color3.fromRGB(45,45,45)
+		  },
+		  ["Ziru"] = {
+			  Image = "118102358638644",
+			  BackgroundTransparency = 0,
+			  BackgroundColor3 = Color3.fromRGB(45,45,45)
+		  },
+		  ["Snake Man"] = {
+			  Image = "118834985644786",
+			  BackgroundTransparency = 0,
+			  BackgroundColor3 = Color3.fromRGB(45,45,45)
+		  },
+		  ["Blossom"] = {
+			  Image = "85463295750056",
+			  BackgroundTransparency = 0,
+			  BackgroundColor3 = Color3.fromRGB(45,45,45)
+		  },
+		  ["Black Crow"] = {
+			  Image = "129187553444710",
+			  BackgroundTransparency = 0,
+			  BackgroundColor3 = Color3.fromRGB(45,45,45)
+		  },
+		  ["Dor"] = {
+			  Image = "88039038571553",
+			  BackgroundTransparency = 0,
+			  BackgroundColor3 = Color3.fromRGB(45,45,45)
+		  }
+	  },
+	HP = {
+		  ["Soondoo"] = "HP: 50",
+		  ["Gonshee"] = "HP: 500",
+		  ["Daek"] = "HP: 5K",
+		  ["LongIn"] = "HP: 10K",
+		  ["Anders"] = "HP: 15K",
+		  ["Largalgan"] = "HP: 30K",
+		  ["Snake Man"] = "HP: 99K",
+		  ["Blossom"] = "HP: 840K"
+	},
+	Island = {
+		  ["Soondoo"] = "Leveling City",
+		  ["Gonshee"] = "Leveling City",
+		  ["Daek"] = "Leveling City",
+		  ["LongIn"] = "Leveling City",
+		  ["Anders"] = "Leveling City",
+		  ["Largalgan"] = "Leveling City",
+		  ["Vermillion"] = "Leveling City",
+		  ["Monarch"] = "Leveling City",
+		  ["Ant Soldier"] = "Dedu Island",
+		  ["Ant Queen"] = "Dedu Island",
+		  ["Ziru"] = "Dedu Island",
+		  ["Snake Man"] = "Grass Village",
+		  ["Blossom"] = "Grass Village",
+		  ["Black Crow"] = "Grass Village",
+		  ["Dor"] = "Grass Village"
+	},
+	Rarity = {
+		  ["Soondoo"] = "Rarity: Common",
+		  ["Gonshee"] = "Rarity: Common",
+		  ["Daek"] = "Rarity: Common",
+		  ["LongIn"] = "Rarity: Common",
+		  ["Anders"] = "Rarity: Common",
+		  ["Largalgan"] = "Rarity: Common",
+		  ["Vermillion"] = "Rarity: Epic",
+		  ["Monarch"] = "Rarity: Legendary",
+		  ["Ant Soldier"] = "Rarity: Common",
+		  ["Ant Queen"] = "Rarity: Epic",
+		  ["Ziru"] = "Rarity: Legendary",
+		  ["Snake Man"] = "Rarity: Common",
+		  ["Blossom"] = "Rarity: Common",
+		  ["Black Crow"] = "Rarity: Common",
+		  ["Dor"] = "Rarity: Epic"
+	}
   }
-}
