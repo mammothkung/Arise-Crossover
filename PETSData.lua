7aa0ae8603883a58dec8fdd591de7ea021439a4f
mammothkung <tongmo.jingithub@gmail.com>
2025-03-31 06:23:46 +0700

    Update PETSData.lua

--- PETSData.lua
+++ PETSData.lua
@@ -29,6 +29,95 @@ return {
             Image = "85957675205976",
             BackgroundTransparency = 0,
             BackgroundColor3 = Color3.fromRGB(45,45,45)
+        },
+        ["Vermillion"] = {
+            Image = "120675730326685",
+            BackgroundTransparency = 0,
+            BackgroundColor3 = Color3.fromRGB(45,45,45)
+        },
+        ["Monarch"] = {
+            Image = "77293116688131",
+            BackgroundTransparency = 0,
+            BackgroundColor3 = Color3.fromRGB(45,45,45)
+        },
+        ["Ant Soldier"] = {
+            Image = "103369685912011",
+            BackgroundTransparency = 0,
+            BackgroundColor3 = Color3.fromRGB(45,45,45)
+        },
+        ["Ant Queen"] = {
+            Image = "73358558923030",
+            BackgroundTransparency = 0,
+            BackgroundColor3 = Color3.fromRGB(45,45,45)
+        },
+        ["Ziru"] = {
+            Image = "118102358638644",
+            BackgroundTransparency = 0,
+            BackgroundColor3 = Color3.fromRGB(45,45,45)
+        },
+        ["Snake Man"] = {
+            Image = "118834985644786",
+            BackgroundTransparency = 0,
+            BackgroundColor3 = Color3.fromRGB(45,45,45)
+        },
+        ["Blossom"] = {
+            Image = 85463295750056",
+            BackgroundTransparency = 0,
+            BackgroundColor3 = Color3.fromRGB(45,45,45)
+        },
+        ["Black Crow"] = {
+            Image = "129187553444710",
+            BackgroundTransparency = 0,
+            BackgroundColor3 = Color3.fromRGB(45,45,45)
+        },
+        ["Dor"] = {
+            Image = "88039038571553",
+            BackgroundTransparency = 0,
+            BackgroundColor3 = Color3.fromRGB(45,45,45)
         }
-    }
+    },
+  HP = {
+        ["Soondoo"] = "HP: 50",
+        ["Gonshee"] = "HP: 500",
+        ["Daek"] = "HP: 5K",
+        ["LongIn"] = "HP: 10K",
+        ["Anders"] = "HP: 15K",
+        ["Largalgan"] = "HP: 30K",
+        ["Snake Man"] = "HP: 99K",
+        ["Blossom"] = "HP: 840K"
+  },
+  Island = {
+        ["Soondoo"] = "Leveling City",
+        ["Gonshee"] = "Leveling City",
+        ["Daek"] = "Leveling City",
+        ["LongIn"] = "Leveling City",
+        ["Anders"] = "Leveling City",
+        ["Largalgan"] = "Leveling City",
+        ["Vermillion"] = "Leveling City",
+        ["Monarch"] = "Leveling City",
+        ["Ant Soldier"] = "Dedu Island",
+        ["Ant Queen"] = "Dedu Island",
+        ["Ziru"] = "Dedu Island",
+        ["Snake Man"] = "Grass Village",
+        ["Blossom"] = "Grass Village",
+        ["Black Crow"] = "Grass Village",
+        ["Dor"] = "Grass Village"
+  },
+  Rarity = {
+        ["Soondoo"] = "Rarity: Common",
+        ["Gonshee"] = "Rarity: Common",
+        ["Daek"] = "Rarity: Common",
+        ["LongIn"] = "Rarity: Common",
+        ["Anders"] = "Rarity: Common",
+        ["Largalgan"] = "Rarity: Common",
+        ["Vermillion"] = "Rarity: Epic",
+        ["Monarch"] = "Rarity: Legendary",
+        ["Ant Soldier"] = "Rarity: Common",
+        ["Ant Queen"] = "Rarity: Epic",
+        ["Ziru"] = "Rarity: Legendary",
+        ["Snake Man"] = "Rarity: Common",
+        ["Blossom"] = "Rarity: Common",
+        ["Black Crow"] = "Rarity: Common",
+        ["Dor"] = "Rarity: Epic"
+  }
 }
