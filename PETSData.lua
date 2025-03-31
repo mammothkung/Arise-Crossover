64458cedb35728c5e0961da688f6523878723d0a
mammothkung <tongmo.jingithub@gmail.com>
2025-03-31 11:02:12 +0700

    Update PETSData.lua

--- PETSData.lua
+++ PETSData.lua
@@ -1,296 +1,34 @@
-return {
-	Pets = {
-		  "Soondoo",
-		  "Gonshee",
-		  "Daek",
-		  "LongIn",
-		  "Anders",
-		  "Largalgan",
-		  "Vermillion",
-		  "Monarch",
-		  "Ant Soldier",
-		  "Ant Queen",
-		  "Ziru",
-		  "Snake Man",
-		  "Blossom",
-		  "Black Crow",
-		  "Dor"
-	},
-	Tier = {
-		Dropdown = {
-			["Soondoo"] = {
-				Type = "Dropdown",
-				Multi = true,
-				List = {"E", "D", "C", "B", "A", "S", "SS", "G", "N"},
-				Default = {"E", "D", "C", "B", "A", "S", "SS", "G", "N"},
-				LayoutOrder = 1
-			},
-			["Gonshee"] = {
-				Type = "Dropdown",
-				Multi = true,
-				List = {"E", "D", "C", "B", "A", "S", "SS", "G", "N"},
-				Default = {"E", "D", "C", "B", "A", "S", "SS", "G", "N"},
-				LayoutOrder = 2
-			},
-			["Daek"] = {
-				Type = "Dropdown",
-				Multi = true,
-				List = {"E", "D", "C", "B", "A", "S", "SS", "G", "N"},
-				Default = {"E", "D", "C", "B", "A", "S", "SS", "G", "N"},
-				LayoutOrder = 3
-			},
-			["LongIn"] = {
-				Type = "Dropdown",
-				Multi = true,
-				List = {"E", "D", "C", "B", "A", "S", "SS", "G", "N"},
-				Default = {"E", "D", "C", "B", "A", "S", "SS", "G", "N"},
-				LayoutOrder = 4
-			},
-			["Anders"] = {
-				Type = "Dropdown",
-				Multi = true,
-				List = {"E", "D", "C", "B", "A", "S", "SS", "G", "N"},
-				Default = {"E", "D", "C", "B", "A", "S", "SS", "G", "N"},
-				LayoutOrder = 5
-			},
-			["Largalgan"] = {
-				Type = "Dropdown",
-				Multi = true,
-				List = {"E", "D", "C", "B", "A", "S", "SS", "G", "N"},
-				Default = {"E", "D", "C", "B", "A", "S", "SS", "G", "N"},
-				LayoutOrder = 6
-			},
-			["Vermillion"] = {
-				Type = "Dropdown",
-				Multi = true,
-				List = {"E", "D", "C", "B", "A", "S", "SS", "G", "N"},
-				Default = {"E", "D", "C", "B", "A", "S", "SS", "G", "N"},
-				LayoutOrder = 7
-			},
-			["Monarch"] = {
-				Type = "Dropdown",
-				Multi = true,
-				List = {"E", "D", "C", "B", "A", "S", "SS", "G", "N"},
-				Default = {"E", "D", "C", "B", "A", "S", "SS", "G", "N"},
-				LayoutOrder = 8
-			},
-			["Ant Soldier"] = {
-				Type = "Dropdown",
-				Multi = true,
-				List = {"E", "D", "C", "B", "A", "S", "SS", "G", "N"},
-				Default = {"E", "D", "C", "B", "A", "S", "SS", "G", "N"},
-				LayoutOrder = 9
-			},
-			["Ant Queen"] = {
-				Type = "Dropdown",
-				Multi = true,
-				List = {"E", "D", "C", "B", "A", "S", "SS", "G", "N"},
-				Default = {"E", "D", "C", "B", "A", "S", "SS", "G", "N"},
-				LayoutOrder = 10
-			},
-			["Ziru"] = {
-				Type = "Dropdown",
-				Multi = true,
-				List = {"E", "D", "C", "B", "A", "S", "SS", "G", "N"},
-				Default = {"E", "D", "C", "B", "A", "S", "SS", "G", "N"},
-				LayoutOrder = 11
-			},
-			["Snake Man"] = {
-				Type = "Dropdown",
-				Multi = true,
-				List = {"E", "D", "C", "B", "A", "S", "SS", "G", "N"},
-				Default = {"E", "D", "C", "B", "A", "S", "SS", "G", "N"},
-				LayoutOrder = 12
-			},
-			["Blossom"] = {
-				Type = "Dropdown",
-				Multi = true,
-				List = {"E", "D", "C", "B", "A", "S", "SS", "G", "N"},
-				Default = {"E", "D", "C", "B", "A", "S", "SS", "G", "N"},
-				LayoutOrder = 13
-			},
-			["Black Crow"] = {
-				Type = "Dropdown",
-				Multi = true,
-				List = {"E", "D", "C", "B", "A", "S", "SS", "G", "N"},
-				Default = {"E", "D", "C", "B", "A", "S", "SS", "G", "N"},
-				LayoutOrder = 14
-			},
-			["Dor"] = {
-				Type = "Dropdown",
-				Multi = true,
-				List = {"E", "D", "C", "B", "A", "S", "SS", "G", "N"},
-				Default = {"E", "D", "C", "B", "A", "S", "SS", "G", "N"},
-				LayoutOrder = 15
-			}
-		},
-		Title = {
-			["Soondoo"] = {
-				Title = "Select Rank"
-			},
-			["Gonshee"] = {
-				Title = "Select Rank"
-			},
-			["Daek"] = {
-				Title = "Select Rank"
-			},
-			["LongIn"] = {
-				Title = "Select Rank"
-			},
-			["Anders"] = {
-				Title = "Select Rank"
-			},
-			["Largalgan"] = {
-				Title = "Select Rank"
-			},
-			["Vermillion"] = {
-				Title = "Select Rank"
-			},
-			["Monarch"] = {
-				Title = "Select Rank"
-			},
-			["Ant Soldier"] = {
-				Title = "Select Rank"
-			},
-			["Ant Queen"] = {
-				Title = "Select Rank"
-			},
-			["Ziru"] = {
-				Title = "Select Rank"
-			},
-			["Snake Man"] = {
-				Title = "Select Rank"
-			},
-			["Blossom"] = {
-				Title = "Select Rank"
-			},
-			["Black Crow"] = {
-				Title = "Select Rank"
-			},
-			["Dor"] = {
-				Title = "Select Rank"
-			}
-		}
-	},
-	Image = {
-		  ["Soondoo"] = {
-			  Image = "83952165808326",
-			  BackgroundTransparency = 0,
-			  BackgroundColor3 = Color3.fromRGB(45,45,45)
-		  },
-		  ["Gonshee"] = {
-			  Image = "82914824031307",
-			  BackgroundTransparency = 0,
-			  BackgroundColor3 = Color3.fromRGB(45,45,45)
-		  },
-		  ["Daek"] = {
-			  Image = "112717350204803",
-			  BackgroundTransparency = 0,
-			  BackgroundColor3 = Color3.fromRGB(45,45,45)
-		  },
-		  ["LongIn"] = {
-			  Image = "107186645910420",
-			  BackgroundTransparency = 0,
-			  BackgroundColor3 = Color3.fromRGB(45,45,45)
-		  },
-		  ["Anders"] = {
-			  Image = "110844883730834",
-			  BackgroundTransparency = 0,
-			  BackgroundColor3 = Color3.fromRGB(45,45,45)
-		  },
-		  ["Largalgan"] = {
-			  Image = "85957675205976",
-			  BackgroundTransparency = 0,
-			  BackgroundColor3 = Color3.fromRGB(45,45,45)
-		  },
-		  ["Vermillion"] = {
-			  Image = "120675730326685",
-			  BackgroundTransparency = 0,
-			  BackgroundColor3 = Color3.fromRGB(45,45,45)
-		  },
-		  ["Monarch"] = {
-			  Image = "77293116688131",
-			  BackgroundTransparency = 0,
-			  BackgroundColor3 = Color3.fromRGB(45,45,45)
-		  },
-		  ["Ant Soldier"] = {
-			  Image = "103369685912011",
-			  BackgroundTransparency = 0,
-			  BackgroundColor3 = Color3.fromRGB(45,45,45)
-		  },
-		  ["Ant Queen"] = {
-			  Image = "73358558923030",
-			  BackgroundTransparency = 0,
-			  BackgroundColor3 = Color3.fromRGB(45,45,45)
-		  },
-		  ["Ziru"] = {
-			  Image = "118102358638644",
-			  BackgroundTransparency = 0,
-			  BackgroundColor3 = Color3.fromRGB(45,45,45)
-		  },
-		  ["Snake Man"] = {
-			  Image = "118834985644786",
-			  BackgroundTransparency = 0,
-			  BackgroundColor3 = Color3.fromRGB(45,45,45)
-		  },
-		  ["Blossom"] = {
-			  Image = "85463295750056",
-			  BackgroundTransparency = 0,
-			  BackgroundColor3 = Color3.fromRGB(45,45,45)
-		  },
-		  ["Black Crow"] = {
-			  Image = "129187553444710",
-			  BackgroundTransparency = 0,
-			  BackgroundColor3 = Color3.fromRGB(45,45,45)
-		  },
-		  ["Dor"] = {
-			  Image = "88039038571553",
-			  BackgroundTransparency = 0,
-			  BackgroundColor3 = Color3.fromRGB(45,45,45)
-		  }
-	  },
-	HP = {
-		  ["Soondoo"] = "HP: 50",
-		  ["Gonshee"] = "HP: 500",
-		  ["Daek"] = "HP: 5K",
-		  ["LongIn"] = "HP: 10K",
-		  ["Anders"] = "HP: 15K",
-		  ["Largalgan"] = "HP: 30K",
-		  ["Snake Man"] = "HP: 99K",
-		  ["Blossom"] = "HP: 840K"
-	},
-	Island = {
-		  ["Soondoo"] = "Leveling City",
-		  ["Gonshee"] = "Leveling City",
-		  ["Daek"] = "Leveling City",
-		  ["LongIn"] = "Leveling City",
-		  ["Anders"] = "Leveling City",
-		  ["Largalgan"] = "Leveling City",
-		  ["Vermillion"] = "Leveling City",
-		  ["Monarch"] = "Leveling City",
-		  ["Ant Soldier"] = "Dedu Island",
-		  ["Ant Queen"] = "Dedu Island",
-		  ["Ziru"] = "Dedu Island",
-		  ["Snake Man"] = "Grass Village",
-		  ["Blossom"] = "Grass Village",
-		  ["Black Crow"] = "Grass Village",
-		  ["Dor"] = "Grass Village"
-	},
-	Rarity = {
-		  ["Soondoo"] = "Rarity: Common",
-		  ["Gonshee"] = "Rarity: Common",
-		  ["Daek"] = "Rarity: Common",
-		  ["LongIn"] = "Rarity: Common",
-		  ["Anders"] = "Rarity: Common",
-		  ["Largalgan"] = "Rarity: Common",
-		  ["Vermillion"] = "Rarity: Epic",
-		  ["Monarch"] = "Rarity: Legendary",
-		  ["Ant Soldier"] = "Rarity: Common",
-		  ["Ant Queen"] = "Rarity: Epic",
-		  ["Ziru"] = "Rarity: Legendary",
-		  ["Snake Man"] = "Rarity: Common",
-		  ["Blossom"] = "Rarity: Common",
-		  ["Black Crow"] = "Rarity: Common",
-		  ["Dor"] = "Rarity: Epic"
-	}
-  }
+local Info = loadstring(game:HttpGet("https://raw.githubusercontent.com/mammothkung/Arise-Crossover/refs/heads/main/PETSInfo.lua"))()
+local Time = 0
+local Data = {
+    Pets = {},
+    Tier = {
+        Title = {},
+        Dropdown = {}
+    },
+    Image = {},
+    Rarity = {}
+}
+
+for i,v in next, Info do
+    Time += 1
+    table.insert(Data.Pets, v.Name)
+    Data.Image[v.Name] = {
+        Image = v.Image,
+        BackgroundTransparency = 0,
+        BackgroundColor3 = Color3.fromRGB(45,45,45)
+    }
+    Data.Rarity[v.Name] = "Rarity: "..v.Rarity
+    Data.Tier.Title[v.Name] = {
+        Title = "Select Rank"
+    }
+    Data.Tier.Dropdown[v.Name] = {
+        Type = "Dropdown",
+        Multi = true,
+        List = {"E", "D", "C", "B", "A", "S", "SS", "G", "N"},
+        Default = {"E", "D", "C", "B", "A", "S", "SS", "G", "N"},
+        LayoutOrder = Time
+    }
+end
+
+return Data
