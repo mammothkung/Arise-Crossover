54ebcb679ac50d9e975a6eb13e82cf2c8ef22757
mammothkung <tongmo.jingithub@gmail.com>
2025-04-16 15:42:05 +0700

    Update ForUInew.lua

--- ForUInew.lua
+++ ForUInew.lua
@@ -1,23 +1,56 @@
+local Map = loadstring(game:HttpGet("https://raw.githubusercontent.com/mammothkung/Arise-Crossover/refs/heads/main/MapDisplay.lua"))()
+local Pet = loadstring(game:HttpGet("https://raw.githubusercontent.com/mammothkung/Arise-Crossover/refs/heads/main/DisplayName.lua"))()
 local Info = loadstring(game:HttpGet("https://raw.githubusercontent.com/mammothkung/Arise-Crossover/refs/heads/main/PETSInfo.lua"))()
 local Time = 0
 local Data = {
     Pets = {
         Sell = {},
-        Enemy = {}
+        Enemy = {
+            ["DBWorld"] = {},
+            ["OPWorld"] = {},
+            ["BCWorld"] = {},
+            ["JojoWorld"] = {},
+            ["SoloWorld"] = {},
+            ["NarutoWorld"] = {},
+            ["BleachWorld"] = {},
+            ["ChainsawWorld"] = {}
+        }
     },
     Image = {}
 }
 
+local function GetPet(n)
+    for i,v in next, Pet do
+        if v.Name == n then
+            return i
+        end
+    end
+end
+local function GetEnemy(n)
+    for i,v in next, Map do
+        if table.find(v.Pets.Normal, n) then
+            return v.Name
+        end
+    end
+end
+
 for i,v in next, Info do
     Time += 1
-    Data.Pets.Sell[v.Name] = {
-        Type = "Dropdown",
-        List = {"E", "D", "C", "B", "A", "S", "SS", "G", "N"},
-        DefaultDropdown = {"E", "D", "C", "B", "A", "S"},
-        DropdownTitle = "Select Rank",
-        Multi = true,
-        LayoutOrder = Time
-    }
+    local b = GetPet(v.Name)
+    local g
+    if b then
+       g = GetEnemy(b)
+    end
+    if g then
+        Data.Pets.Sell[g][v.Name] = {
+            Type = "Dropdown",
+            List = {"E", "D", "C", "B", "A", "S", "SS", "G", "N"},
+            DefaultDropdown = {"E", "D", "C", "B", "A", "S"},
+            DropdownTitle = "Select Rank",
+            Multi = true,
+            LayoutOrder = Time
+        }
+    end
     Data.Pets.Enemy[v.Name] = {
         Type = {"Dropdown", "Slider"},
         SliderTitle = "Select Priority",
