0783ae03a39804d41d483ab682088acd903cc253
mammothkung <tongmo.jingithub@gmail.com>
2025-04-17 18:46:30 +0700

    Update and rename ShadowData.lua to GameData.lua

--- GameData.lua
+++ GameData.lua
@@ -2,7 +2,9 @@ local EId = loadstring(game:HttpGet("https://raw.githubusercontent.com/mammothku
 local Map = loadstring(game:HttpGet("https://raw.githubusercontent.com/mammothkung/Arise-Crossover/refs/heads/main/MapDisplay.lua"))()
 local Pet = loadstring(game:HttpGet("https://raw.githubusercontent.com/mammothkung/Arise-Crossover/refs/heads/main/DisplayName.lua"))()
 local Info = loadstring(game:HttpGet("https://raw.githubusercontent.com/mammothkung/Arise-Crossover/refs/heads/main/PETSInfo.lua"))()
+local Rune = loadstring(game:HttpGet("https://raw.githubusercontent.com/mammothkung/Arise-Crossover/refs/heads/main/RuneList.lua"))()
 local Time = 0
+local x0uz = 0
 local Data = {
     Pets = {
         Sell = {},
@@ -18,8 +20,16 @@ local Data = {
             ["Nipon City"] = {}
         }
     },
+    Rune = {
+        Data = {},
+        Image = {}
+    },
     Image = {}
 }
+local Kw = {}
+for i,v in next, Map do
+    table.insert(Kw, v.Name)
+end
 
 local function GetPet(n)
     for i,v in next, Pet do
@@ -36,6 +46,26 @@ local function GetEnemy(n)
     end
 end
 
+for i,v in next, Rune do
+    x0uz += 1
+    Data.Rune.Data[v.Name] = {
+        Type = {"Dropdown", "Dropdown2"},
+        List = {"E", "D", "C", "B", "A", "S", "SS"},
+        DropdownTitle = "Select Rank",
+        DropdownTitle2 = "Select World",
+        List2 = Kw,
+        Multi = true,
+        Multi2 = true,
+        DefaultDropdown = {"E", "D", "C", "B", "A"},
+        DefaultDropdown2 = {"Leveling City", "Brum Island", "Grass Village"},
+        LayoutOrder = x0uz
+    }
+    Data.Rune.Image[v.Name] = {
+        Image = v.Image,
+        Color = false,
+        BackgroundTransparency = 1
+    }
+end
 for i,v in next, Info do
     Time += 1
     local b = GetPet(v.Name)
