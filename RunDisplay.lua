da7d5d7f149df1105e9ce5e35c86c0d3320e90b0
mammothkung <tongmo.jingithub@gmail.com>
2025-04-17 18:36:52 +0700

    Create RunDisplay.lua

---
+++ RunDisplay.lua
@@ -0,0 +1,114 @@
+return {
+	DgSoloRune = {
+		Name = "Solo Rune";
+		Image = "rbxassetid://90635220598465";
+		Type = "DungeonItem";
+		Max = 10;
+		Desc = "Changes the boss and some of the dungeon enemies to be Solo City-themed.";
+		Dungeon = {
+			Unique = true;
+			TypeUnique = true;
+			Type = "WorldRune";
+			World = "SoloWorld";
+		};
+		Rarity = "Rare";
+	};
+	DgNarutoRune = {
+		Name = "Grass Rune";
+		Image = "rbxassetid://134577572174039";
+		Type = "DungeonItem";
+		Max = 10;
+		Desc = "Changes the boss and some of the dungeon enemies to be Grass Village-themed.";
+		Dungeon = {
+			Unique = true;
+			TypeUnique = true;
+			Type = "WorldRune";
+			World = "NarutoWorld";
+		};
+		Rarity = "Rare";
+	};
+	DgOPRune = {
+		Name = "Brum Rune";
+		Image = "rbxassetid://107822025601990";
+		Type = "DungeonItem";
+		Max = 10;
+		Desc = "Changes the boss and some of the dungeon enemies to be Brum Island-themed.";
+		Dungeon = {
+			Unique = true;
+			TypeUnique = true;
+			Type = "WorldRune";
+			World = "OPWorld";
+		};
+		Rarity = "Rare";
+	};
+	DgBleachRune = {
+		Name = "Faceheal Rune";
+		Image = "rbxassetid://71855458073519";
+		Type = "DungeonItem";
+		Max = 10;
+		Desc = "Changes the boss and some of the dungeon enemies to be Faceheal-themed.";
+		Dungeon = {
+			Unique = true;
+			TypeUnique = true;
+			Type = "WorldRune";
+			World = "BleachWorld";
+		};
+		Rarity = "Rare";
+	};
+	DgBCRune = {
+		Name = "Lucky Rune";
+		Image = "rbxassetid://71007282332500";
+		Type = "DungeonItem";
+		Max = 10;
+		Desc = "Changes the boss and some of the dungeon enemies to be Lucky Kingdom-themed.";
+		Dungeon = {
+			Unique = true;
+			TypeUnique = true;
+			Type = "WorldRune";
+			World = "BCWorld";
+		};
+		Rarity = "Rare";
+	};
+	DgChainsawRune = {
+		Name = "Nipon Rune";
+		Image = "rbxassetid://99707824286950";
+		Type = "DungeonItem";
+		Max = 10;
+		Desc = "Changes the boss and some of the dungeon enemies to be Nipon City-themed.";
+		Dungeon = {
+			Unique = true;
+			TypeUnique = true;
+			Type = "WorldRune";
+			World = "ChainsawWorld";
+		};
+		Rarity = "Rare";
+	};
+	DgJojoRune = {
+		Name = "Mori Rune";
+		Image = "rbxassetid://138559466857104";
+		Type = "DungeonItem";
+		Max = 10;
+		Desc = "Changes the boss and some of the dungeon enemies to be Mori Town-themed.";
+		Dungeon = {
+			Unique = true;
+			TypeUnique = true;
+			Type = "WorldRune";
+			World = "JojoWorld";
+		};
+		Rarity = "Rare";
+	};
+	DgDbRune = {
+		Name = "Dragon Rune";
+		Image = "rbxassetid://136044338032926";
+		Type = "DungeonItem";
+		Max = 10;
+		Desc = "Changes the boss and some of the dungeon enemies to be Dragon City-themed.";
+		Dungeon = {
+			Unique = true;
+			TypeUnique = true;
+			Type = "WorldRune";
+			World = "DBWorld";
+		};
+		Rarity = "Rare";
+	};
+}
